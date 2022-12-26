cdb18848f17de2b0992907d090a96ff8a9e10ec9
Renardi <renardi.adryantoro@geniebook.com>
2022-12-26 10:00:32 +0700

    Update motion

--- motion.py
+++ motion.py
@@ -5,10 +5,7 @@ import math
 
 
 
-def geometric_invers_kinematic(xref,yref,zref):
-    A1 = 0
-    A2 = 2
-    A3 = 5
+def geometric_invers_kinematic(xref,yref,zref,A1,A2,A3):
 
     if xref > 0:
         theta1 = math.degrees(math.atan(yref/xref))
@@ -20,6 +17,7 @@ def geometric_invers_kinematic(xref,yref,zref):
     r_3 = math.sqrt(r_2*r_2 + r_1*r_1)
 
     psi_1 = math.degrees(math.atan(r_2/r_1))
+    print((A2*A2 + r_3*r_3 + - A3*A3)/( 2 *A2 * r_3 ))
     psi_2 = math.degrees(math.acos((A2*A2 + r_3*r_3 + - A3*A3)/( 2 *A2 * r_3 )))
     theta2 = psi_1+psi_2
 
@@ -27,11 +25,6 @@ def geometric_invers_kinematic(xref,yref,zref):
     theta3 = -(180-psi_3)
     
     return theta1,theta2,theta3
-
-
-
-
-
 
 
 
@@ -49,21 +42,24 @@ def arm_draw(point):
 
 
 #robot input
-point_baseframe = [0,0,5]
-arm_length_1 = 5
+point_baseframe = [0,0,0]
+point_eof = [5,5,0]
+
+arm_length_1 = 1
 arm_length_2 = 5
-angle_1 = 45
-angle_2 = 45
+arm_length_3 = 5
 
+angle_1,angle_2,angle_3 = geometric_invers_kinematic(point_eof[0],point_eof[1],point_eof[2],arm_length_1,arm_length_2,arm_length_3)
+print(angle_1,angle_2,angle_3)
 
-x_point_2 = point_baseframe[0]+arm_length_1*np.sin(np.deg2rad(angle_2))*np.cos(np.deg2rad(angle_1))
-y_point_2 = point_baseframe[1]+arm_length_1*np.sin(np.deg2rad(angle_2))*np.sin(np.deg2rad(angle_1))
-z_point_2 = point_baseframe[2]+arm_length_1*np.sin(np.deg2rad(angle_2))
+x_point_1 = point_baseframe[0]
+y_point_1 = point_baseframe[1]
+z_point_1 = point_baseframe[2]+arm_length_1
 
-x_point_3 = x_point_2+arm_length_2*np.sin(np.deg2rad(angle_2))*np.cos(np.deg2rad(angle_1))
-y_point_3 = y_point_2+arm_length_2*np.sin(np.deg2rad(angle_2))*np.sin(np.deg2rad(angle_1))
-z_point_3 = z_point_2+arm_length_2*np.sin(np.deg2rad(angle_2))
+x_point_2 = point_baseframe[0]+arm_length_2*np.sin(np.deg2rad(angle_2))*np.cos(np.deg2rad(angle_1))
+y_point_2 = point_baseframe[1]+arm_length_2*np.sin(np.deg2rad(angle_2))*np.sin(np.deg2rad(angle_1))
+z_point_2 = point_baseframe[2]+arm_length_2*np.sin(np.deg2rad(angle_2))
 
 #input
-point=[point_baseframe, [x_point_2,y_point_2,z_point_2]]
+point=[point_baseframe,[x_point_1,y_point_1,z_point_1],[x_point_2,y_point_2,z_point_2], point_eof]
 arm_draw(point)
